37808b07e324dee12fd9b93e6e9cf225614d4171
Farhan Tanvir <32593150+Farhandroid@users.noreply.github.com>
2019-09-13 19:51:34 +0900

    Category crud done . Post functionaly image upload done

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\Posts\CreatePostRequest;
+use App\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('posts.index')->with('posts',Post::all());
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('posts.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(CreatePostRequest $request)
+    {
+    
+        $image= $request->image->store('posts');
+        $data=request()->all();
+        $post=new Post();
+        $post->title=$data['title'];
+        $post->description=$data['description'];
+        $post->content=$data['content'];
+        $post->image=$image;
+        $post->save();
+        session()->flash('success','Post Created successfully');
+        return redirect(route('posts.index'));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ resources/views/categories/index.blade.php
@@ -0,0 +1,74 @@
+@extends('layouts.app')
+
+
+@section('content')
+<div class='d-flex justify-content-end mb-2'>
+    <a href="{{ route('categories.create') }}" class="btn btn-success">Add Category</a>
+</div>
+
+<div class="card card-drfault">
+    <div class="card-header">Categories</div>
+    <div class="card-body">
+        <table class="table">
+            <thead>
+                <th>Name</th>
+                <th></th>
+            </thead>
+
+            <tbody>
+                @foreach($categories as $categorie)
+                    <tr>
+                        <td>
+                            {{$categorie->name }}
+                        </td>
+                        <td>
+                        <a href="{{ route('categories.edit',$categorie->id )}}" class="btn btn-info">Edit</a>
+                        <button class="btn btn-danger" onclick="handleDelete({{$categorie->id}})">Delete</button>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+
+        <form id="deleteCategoryForm" method="POST">
+            @csrf
+            @method('DELETE')
+                <div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="deleteModalLabel" aria-hidden="true">
+                        <div class="modal-dialog" role="document">
+                          <div class="modal-content">
+                            <div class="modal-header">
+                              <h5 class="modal-title" id="deleteModalLabel">Delete Category</h5>
+                              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                              </button>
+                            </div>
+                            <div class="modal-body">
+                                <p class="text-center text-bold">
+                                        Are you sure you want to delete this Category ?
+                                </p>
+                            </div>
+                            <div class="modal-footer">
+                              <button type="button" class="btn btn-primary" data-dismiss="modal">No, Go back</button>
+                              <button type="submit" class="btn btn-danger">Yes , Delete</button>
+                            </div>
+                          </div>
+                        </div>
+                </div>
+        </form>
+    </div>
+</div>
+@endsection
+
+@section('scripts')
+
+<script>
+    function handleDelete(id)
+    {
+        //console.log("delete");
+        ///console.log(id);
+        var form=document.getElementById('deleteCategoryForm')
+        form.action='/categories/'+id
+        $('#deleteModal').modal('show')
+    }
+</script>
+    
+@endsection
